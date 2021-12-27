a6918fb67c31ffe4f194f76abfe4dd4da58304ff
Bicet <bicets@gmail.com>
2021-12-27 17:46:58 +0100

    sm8250-common: Switch to Neutrino kernel
    old but gold

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -57,7 +57,7 @@ TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := liyuu
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-liyuu
-TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
+TARGET_KERNEL_CONFIG := neutrino_defconfig
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
