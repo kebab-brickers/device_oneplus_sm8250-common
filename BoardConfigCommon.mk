50bcfda6ca3aa427b655f89ceb60ef07298b0d14
Bicet <bicets@gmail.com>
2022-01-03 07:19:41 +0100

    sm8250-common: switch to Psycho config

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -56,7 +56,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/
 TARGET_KERNEL_ADDITIONAL_FLAGS += AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8250
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := vendor/kona-perf_defconfig
+TARGET_KERNEL_CONFIG := vendor/kona-full_defconfig
 TARGET_KERNEL_CLANG_VERSION := liyuu
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-liyuu
 
